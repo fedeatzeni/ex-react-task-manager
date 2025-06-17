fa919149697d989f92b9a1544bdcd4507f6b15a8
Fede <fede.atzeni372@gmail.com>
2025-06-17 13:39:50 +0200

    feat milestone 11

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useMemo, useState } from "react"
 import GlobalContext from "../contexts/GlobalContext"
 
 import NavBar from "../components/NavBar"
@@ -11,9 +11,50 @@ export default function TaskList() {
     const { tasks } = useTasks()
     // console.log(tasks);
 
+    //sort
+    const [sortBy, setSortBy] = useState("createdAt")
+    const [sortOrder, SetSortOrdere] = useState(1)
+
+    function handleSort(value) {
+        if (sortBy === value) SetSortOrdere(sortOrder * -1)
+        else setSortBy(value)
+    }
+    // console.log(sortBy, sortOrder);
+
+    const sort = useMemo(() => {
+
+        if (sortBy === "title") {
+            tasks.sort((a, b) => {
+                return a.title.localeCompare(b.title) * sortOrder;
+            });
+        }
+
+        if (sortBy === "status") {
+            tasks.sort((a, b) => {
+                const statusOrder = ["To do", "Doing", "Done"];
+                const indexA = statusOrder.indexOf(a.status);
+                const indexB = statusOrder.indexOf(b.status);
+                return (indexA - indexB) * sortOrder;
+            });
+        }
+
+        if (sortBy === "createdAt") {
+            tasks.sort((a, b) => {
+                return (new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()) * sortOrder;
+            });
+        }
+
+    }, [tasks, sortBy, sortOrder])
+
+
     return (
         <>
             <NavBar />
+            <div className={`task-row`} >
+                <div className="name" onClick={() => handleSort("title")}>Titolo</div>
+                <div onClick={() => handleSort("status")}>Stato</div>
+                <div onClick={() => handleSort("createdAt")}>Data di creazione</div>
+            </div>
             <div>
                 {tasks.map(task => (
                     <TaskRow key={task.id} task={task} />
